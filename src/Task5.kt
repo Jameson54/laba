b92f44caadab48e3551b4ca9f59a653247aeeefd
Jameson54 <hehlol123123@mail.ru>
2026-09-23 13:57:18 +0700

    Update Task5.kt

--- src/Task5.kt
+++ src/Task5.kt
@@ -1,7 +1,6 @@
 fun main() {
     val array = intArrayOf(1, 3, 2, 5, 4, 7, 6, 9, 8)
 
-    // С использованием цикла for
     println("С использованием for:")
     for (i in 1 until array.size - 1) {
         if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
@@ -9,7 +8,6 @@ fun main() {
         }
     }
 
-    // С использованием цикла while
     println("С использованием while:")
     var i = 1
     while (i < array.size - 1) {
@@ -18,8 +16,7 @@ fun main() {
         }
         i++
     }
-
-    // С использованием forEach
+h
     println("С использованием forEach:")
     array.forEachIndexed { index, value ->
         if (index in 1 until array.size - 1) {
